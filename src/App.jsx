e9876cb53f7c8f4362f981b919cf41355cd8df6d
Valentyn Zozulia <valikobird@protonmail.com>
2025-03-12 13:42:38 +0100

    Implement generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+import text from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum</h2>;
+  const [textLines, setTextLines] = useState(text);
+  const [counter, setCounter] = useState(1);
+  const [textToShow, setTextToShow] = useState([]);
+
+  const handleGenerate = (e) => {
+    e.preventDefault();
+
+    const newText = textLines.slice(0, counter);
+    setTextToShow(newText);
+  };
+
+  return (
+    <main>
+      <form className='form'>
+        <h2>tired of boring lorem ipsum?</h2>
+        <div className='formRow'>
+          <label htmlFor='count'>Paragraphs:</label>
+          <input
+            type='number'
+            id='count'
+            min={1}
+            max={text.length}
+            value={counter}
+            onChange={(e) => setCounter(+e.target.value)}
+          />
+          <button type='submit' className='btn' onClick={handleGenerate}>
+            generate
+          </button>
+        </div>
+      </form>
+      <section className='resultContainer'>
+        {textToShow.map((line, index) => {
+          return (
+            <p className='textLine' key={index}>
+              {line}
+            </p>
+          );
+        })}
+      </section>
+    </main>
+  );
 };
 
 export default App;
